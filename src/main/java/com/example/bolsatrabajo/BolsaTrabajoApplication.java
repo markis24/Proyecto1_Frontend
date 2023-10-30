3eac9a8ede9ea8fdb3b06cbd6c90ff5178931f72
Jostin Fabian <Jostinfabian14@gmail.com>
2023-10-30 15:42:20 +0100

    Añadida la funcionalidad de consultar una empresa

---
+++ src/main/java/com/example/bolsatrabajo/BolsaTrabajoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bolsatrabajo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolsaTrabajoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolsaTrabajoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bolsatrabajo/BolsaTrabajoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bolsatrabajo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolsaTrabajoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
